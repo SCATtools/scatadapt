cf1c9d650431480431a5a4257c535cd9d8c986c2
thirtysixth <65007080+thirtysixth@users.noreply.github.com>
2021-08-02 21:27:30 +0300

    Update scatadapt.py

--- scatadapt.py
+++ scatadapt.py
@@ -10,12 +10,6 @@ def Ji(th,it,model=None, D=1):
     ji = dP * d2P/(P * Q)
     dji = (P * Q * (d2P**2 + dP * d3P) - dP**2 * d2P *(Q - P))/(P**2 * Q**2)
     res = {'ji': ji, 'dji' : dji}
-  else:
-    prov=dP*d2P/P
-    prov1=(P*d2P**2+P*dP*d3P-dP**2*d2P)/P**2
-    res=as.numeric(rowSums(prov,na.rm=TRUE))
-    resd=as.numeric(rowSums(prov1,na.rm=TRUE))
-    res={Ji:res,dJi:resd}
   return res
 def Pi():
   
