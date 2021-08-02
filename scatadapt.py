d1e9dd278aab1779171eb5a4340397ab8d5bb039
thirtysixth <65007080+thirtysixth@users.noreply.github.com>
2021-08-02 21:33:24 +0300

    Update scatadapt.py

--- scatadapt.py
+++ scatadapt.py
@@ -10,6 +10,11 @@ def Ji(th,it,model=None, D=1):
     ji = dP * d2P/(P * Q)
     dji = (P * Q * (d2P**2 + dP * d3P) - dP**2 * d2P *(Q - P))/(P**2 * Q**2)
     res = {'ji': ji, 'dji' : dji}
+  else:
+    raise Exception('Не реализовано!')
   return res
+def fullDist(th, it, method = "BM", priorDist="norm",priorPar=np.array([0,1]), weight = "Huber", tuCo   = 1, range  = np.array([-4 ,4]), parInt = np.array([-4, 4, 33])):
+  
+             
 def Pi():
   
